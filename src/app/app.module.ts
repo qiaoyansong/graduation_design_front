726c99cb1af4207b3f0d770e7fd3a30ecfc76af2
qiaoyansong <1012077930@qq.com>
2020-09-28 21:01:06 +0800

    引入相关module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzBackTopModule } from 'ng-zorro-antd/back-top';
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgZorroAntdModule
+    NzButtonModule,
+    BrowserAnimationsModule,
+    NzLayoutModule,
+    NzBackTopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
