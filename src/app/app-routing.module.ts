34ae3a9e55f71ebf1ebdb06f817153f0502abeb7
qiaoyansong <1012077930@qq.com>
2020-10-06 11:38:54 +0800

    添加路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarouselComponent } from './carousel/carousel.component';
+import { TinyEditorComponent } from './tiny-editor/tiny-editor.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'tinyEditor', component: TinyEditorComponent},
+  {path: '',   redirectTo: '/tinyEditor', pathMatch: 'full'},
+  {path: 'caroul', component: CarouselComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
